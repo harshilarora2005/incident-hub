86568a358ccdc2cfe07d5766c261e1a5038b9094
harshilarora2005 <harshil.arora205@gmail.com>
2026-06-04 11:21:07 +0530

    made LoginRequest dto

--- backend/src/main/java/com/example/backend/dtos/LoginRequest.java
+++ backend/src/main/java/com/example/backend/dtos/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.example.backend.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
 }
