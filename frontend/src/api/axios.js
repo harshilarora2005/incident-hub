ad3318781bfafd39609924eaa618997ae2404800
harshilarora2005 <harshil.arora205@gmail.com>
2026-06-05 10:58:43 +0530

    removed local storage issue with frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,33 +1,23 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: '/api',
-    headers: { 
-        'Content-Type': 'application/json' 
-    }
-})
-
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token')
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-})
+    baseURL: "/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true,
+});
 
 api.interceptors.response.use(
-    res => res,
-    err => {
-        const token = localStorage.getItem('token');
+    (response) => response,
+    (error) => {
 
-        if (err.response?.status === 401 && token) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            window.location.href = '/login';
+        if (error.response?.status === 401) {
+            window.location.href = "/login";
         }
 
-        return Promise.reject(err);
+        return Promise.reject(error);
     }
 );
 
-export default api
+export default api;
