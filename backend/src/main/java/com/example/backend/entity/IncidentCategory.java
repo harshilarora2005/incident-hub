ae5a614fe934dc717a402c0c03c6543ce78b5ee9
harshilarora2005 <harshil.arora205@gmail.com>
2026-06-09 14:51:23 +0530

    created two enums for incident category and severity

---
+++ backend/src/main/java/com/example/backend/entity/IncidentCategory.java
@@ -0,0 +1,5 @@
+package com.example.backend.entity;
+
+public enum IncidentCategory {
+    INFRA, SECURITY, NETWORK, APP, DATABASE, GENERAL
+}
---
+++ backend/src/main/java/com/example/backend/entity/IncidentSeverity.java
@@ -0,0 +1,5 @@
+package com.example.backend.entity;
+
+public enum IncidentSeverity {
+    CRITICAL, HIGH, MEDIUM, LOW
+}
